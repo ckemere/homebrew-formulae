9752f513f3d61b7febb99f0b0f8bb83363223900
ckemere <ckemere@gmail.com>
2023-08-07 13:59:47 -0500

    Update arm-none-eabi-gcc.rb
    Fixed path

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -9,7 +9,7 @@ class ArmNoneEabiGcc < Formula
   sha256 '91b46a984743ac2323376dd0273ad587d56c74253315ede6fcc1cc8056b2aa85'
 
   def install
-    (prefix/"arm-gnu-toolchain-12.3.rel1-darwin-arm64-arm-none-eabi").install Dir["./*"]
-    Dir.glob(prefix/"arm-gnu-toolchain-12.3.rel1-darwin-arm64-arm-none-eabi/bin/*") { |file| bin.install_symlink file }
+    (prefix/"gcc").install Dir["./*"]
+    Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
   end
 end
